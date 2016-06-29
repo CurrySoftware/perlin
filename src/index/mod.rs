9f8588be36d8bfe66b265c8c2fbe1a7a4ed1c659
Jakob Demler <jdemler@curry-software.com>
2016-06-29 11:27:14 +0200

    Introduced Index-Trait

---
+++ src/index/mod.rs
@@ -0,0 +1,10 @@
+pub trait Index<TTerm> {
+    type Query;
+    type QueryResult;
+    
+    fn new() -> Self;
+
+    fn index_document<TDocIterator: Iterator<Item=TTerm>>(&mut self, document: TDocIterator) -> usize;
+
+    fn execute_query(&self, query: Self::Query) -> Self::QueryResult;
+}
