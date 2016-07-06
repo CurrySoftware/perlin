f24e6470bb153ccae44d02a59e547d6e0b9f4e82
Jakob Demler <jdemler@curry-software.com>
2016-07-06 10:24:18 +0200

    PersistentIndex Trait

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,3 +1,6 @@
+use std::io::Result;
+use std::path::Path;
+
 pub mod boolean_index;
 
 pub trait Index<TTerm> {
@@ -9,4 +12,10 @@ pub trait Index<TTerm> {
     fn index_document<TDocIterator: Iterator<Item=TTerm>>(&mut self, document: TDocIterator) -> usize;
 
     fn execute_query(&self, query: &Self::Query) -> Self::QueryResult;
+}
+
+pub trait PersistentIndex {
+    fn write_to_file(&self, path: &Path) -> Result<()>;
+
+    fn read_from_file(path: &Path) -> Self;
 }
