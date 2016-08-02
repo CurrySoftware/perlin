7a59248678d3832235ac0b9217a61ca19a7d989b
Jakob Demler <jdemler@curry-software.com>
2016-08-02 11:12:37 +0200

    fixed docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Perlin is a free and open source information retrieval library.
 //!
 //! Version 0.0.0 (not released)
-//! 
+//!
 //! # Features
 //!
 //! # Basic Usage
@@ -12,7 +12,7 @@
 //! perlin = "0.0"
 //! ```
 //! and import it in your crate root:
-//! 
+//!
 //! ```rust
 //! extern crate perlin;
 //! ```
@@ -20,16 +20,20 @@
 //!
 //! ## Indexing documents and run queries
 //! ```rust
+//! use std::path::Path;
 //! use perlin::index::Index;
-//! use perlin::index::boolean_index::BooleanIndex;
-//! // Create a new index:
-//! let mut index = BooleanIndex::new();
-//! index.index_document(0..10);
-//! index.index_document(5..15);
+//! use perlin::index::boolean_index::{BooleanIndex, RamPostingProvider, FsPostingProvider};
+
+//! //Create a new index and tell it where to store its data.
+//! //In this case we will tell it to store its data in memory
+//! let mut index = BooleanIndex::new(Box::new(RamPostingProvider::new()));
+//! //If you want it to store data on disk, because you have limited RAM or lots of data
+//! //use FsPostingProvider like
+//! let mut fs_index: BooleanIndex<usize> = BooleanIndex::new(
+//!                           Box::new(FsPostingProvider::new(Path::new("/tmp/index.bin"))));
+//! index.index_documents(vec![(0..10), (0..15), (10..34)]);
 //! ```
 //!
-
-
 
 
 pub mod index;
