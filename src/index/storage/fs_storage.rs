8515a5db8399f30fc5b55a3cb81811b26a3f2c7d
Jakob Demler <jdemler@curry-software.com>
2016-08-28 12:41:01 +0200

    Make fs_storage persistent

--- src/index/storage/fs_storage.rs
+++ src/index/storage/fs_storage.rs
@@ -11,25 +11,73 @@ use index::storage::{Result, Storage, StorageError};
 use index::boolean_index::posting::Posting;
 use utils::compression::{vbyte_encode, VByteDecoder};
 
-
 pub struct FsPostingStorage {
     // Stores for every id the offset in the file and the length
-    data: BTreeMap<u64, (u64, u32)>,
-    dir: File,
-    offset: u64,
+    entries: BTreeMap<u64, (u64 /* offset */, u32 /* length */)>,
+    persistent_entries: File,
+    data: File,
+    current_offset: u64,
 }
 
 impl FsPostingStorage {
+    /// Creates a new and empty instance of FsPostingStorage
     pub fn new(path: &Path) -> Self {
+        assert!(path.is_dir(),
+                "FsStorage::new expects a directory not a file!");
         FsPostingStorage {
-            offset: 0,
-            data: BTreeMap::new(),
-            dir: OpenOptions::new()
+            current_offset: 0,
+            entries: BTreeMap::new(),
+            persistent_entries: OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(path.join("entries.bin"))
+                .unwrap(),
+            data: OpenOptions::new()
                 .read(true)
                 .write(true)
                 .create(true)
                 .truncate(true)
-                .open(path)
+                .open(path.join("data.bin"))
+                .unwrap(),
+        }
+    }
+
+    /// Reads a FsStorage from an previously populated folder.
+    // TODO: Return result
+    pub fn from_folder(path: &Path) -> Self {
+        // Read from entry file to BTreeMap.
+        let mut entries = BTreeMap::new();
+        // 1. Open file
+        let mut entries_file =
+            OpenOptions::new().read(true).open(path.join("entries.bin")).unwrap();
+        let mut bytes = Vec::with_capacity(entries_file.metadata().unwrap().len() as usize);
+        // 2. Read file
+        assert!(entries_file.read_to_end(&mut bytes).is_ok());
+        let mut decoder = VByteDecoder::new(bytes.into_iter());
+        // 3. Decode entries and write them to BTreeMap
+        while let Some(entry) = decode_entry(&mut decoder) {
+            entries.insert(entry.0, (entry.1, entry.2));
+        }
+
+        //Get data file length for offset
+        let offset = File::open(path.join("data.bin"))
+            .unwrap()
+            .metadata()
+            .unwrap()
+            .len();
+
+        FsPostingStorage {
+            current_offset: offset,
+            entries: entries,
+            persistent_entries: OpenOptions::new()
+                .append(true)
+                .open(path.join("entries.bin"))
+                .unwrap(),
+            data: OpenOptions::new()
+                .read(true)
+                .append(true)
+                .open(path.join("data.bin"))
                 .unwrap(),
         }
     }
@@ -38,8 +86,8 @@ impl FsPostingStorage {
 
 impl Storage<Vec<Posting>> for FsPostingStorage {
     fn get(&self, id: u64) -> Result<Arc<Vec<Posting>>> {
-        let posting_offset = self.data.get(&id).unwrap();
-        let mut f = self.dir.try_clone().unwrap();
+        let posting_offset = self.entries.get(&id).unwrap();
+        let mut f = self.data.try_clone().unwrap();
         f.seek(SeekFrom::Start(posting_offset.0)).unwrap();
         let mut bytes = vec![0; posting_offset.1 as usize];
         f.read_exact(&mut bytes).unwrap();
@@ -51,16 +99,22 @@ impl Storage<Vec<Posting>> for FsPostingStorage {
     }
 
     fn store(&mut self, id: u64, data: Vec<Posting>) -> Result<()> {
-        //Encode the data
+        // Encode the data
         let bytes = encode_listing(id, &data);
-        //Append it to the file
-        if let Err(e) = self.dir.write_all(&bytes) {
-            return Err(StorageError::WriteError(Some(e)))
+        // Append it to the file
+        if let Err(e) = self.data.write_all(&bytes) {
+            return Err(StorageError::WriteError(Some(e)));
         }
-        //And save the offset and the number of bytes written for later recovery
-        self.data.insert(id, (self.offset, bytes.len() as u32));
-        //Update offset
-        self.offset += bytes.len() as u64;
+        // And save the offset and the number of bytes written for later recovery
+        self.entries.insert(id, (self.current_offset, bytes.len() as u32));
+        // Also write the id, offset and number of bytes written to file for persistence
+        let entry_bytes = encode_entry(id, self.current_offset, bytes.len() as u32);
+        if let Err(e) = self.persistent_entries.write_all(&entry_bytes) {
+            return Err(StorageError::WriteError(Some(e)));
+        }
+
+        // Update offset
+        self.current_offset += bytes.len() as u64;
         Ok(())
     }
 }
@@ -84,6 +138,20 @@ fn decode_listing(mut decoder: VByteDecoder) -> Vec<Posting> {
     postings
 }
 
+fn encode_entry(id: u64, offset: u64, length: u32) -> Vec<u8> {
+    let mut bytes: Vec<u8> = Vec::new();
+    bytes.append(&mut vbyte_encode(id as usize));
+    bytes.append(&mut vbyte_encode(offset as usize));
+    bytes.append(&mut vbyte_encode(length as usize));
+    bytes
+}
+
+fn decode_entry(decoder: &mut VByteDecoder) -> Option<(u64, u64, u32)> {
+    let id = try_option!(decoder.next()) as u64;
+    let offset = try_option!(decoder.next()) as u64;
+    let length = try_option!(decoder.next()) as u32;
+    Some((id, offset, length))
+}
 
 fn encode_listing(term_id: u64, listing: &[Posting]) -> Vec<u8> {
     let mut bytes: Vec<u8> = Vec::new();
@@ -101,15 +169,57 @@ fn encode_listing(term_id: u64, listing: &[Posting]) -> Vec<u8> {
     bytes
 }
 
-#[test]
-pub fn fs_provider() {
-    let posting1 = vec![(10, vec![0, 1, 2, 3, 4]), (1, vec![15])];
-    let posting2 = vec![(0, vec![0, 1, 4]), (1, vec![5, 15566, 3423565]), (5, vec![0, 24, 56])];
-    let mut prov = FsPostingStorage::new(Path::new("/tmp/test_index.bin"));
-    prov.store(0, posting1.clone());
-    assert_eq!(prov.get(0).unwrap().as_ref(), &posting1);
-    prov.store(1, posting2.clone());
-    assert_eq!(prov.get(1).unwrap().as_ref(), &posting2);
-    assert!(prov.get(0).unwrap().as_ref() != &posting2);
-    assert_eq!(prov.get(0).unwrap().as_ref(), &posting1);
+
+
+#[cfg(test)]
+mod tests {
+    use std::fs::create_dir_all;
+    use std::path::Path;
+
+    use super::*;
+    use index::storage::Storage;
+
+
+    #[test]
+    pub fn basic() {
+        let posting1 = vec![(10, vec![0, 1, 2, 3, 4]), (1, vec![15])];
+        let posting2 = vec![(0, vec![0, 1, 4]), (1, vec![5, 15566, 3423565]), (5, vec![0, 24, 56])];
+        assert!(create_dir_all(Path::new("/tmp/test_index")).is_ok());
+        let mut prov = FsPostingStorage::new(Path::new("/tmp/test_index"));
+        assert!(prov.store(0, posting1.clone()).is_ok());
+        assert_eq!(prov.get(0).unwrap().as_ref(), &posting1);
+        assert!(prov.store(1, posting2.clone()).is_ok());
+        assert_eq!(prov.get(1).unwrap().as_ref(), &posting2);
+        assert!(prov.get(0).unwrap().as_ref() != &posting2);
+        assert_eq!(prov.get(0).unwrap().as_ref(), &posting1);
+    }
+
+    #[test]
+    pub fn persistence() {
+        let posting1 = vec![(10, vec![0, 1, 2, 3, 4]), (1, vec![15])];
+        let posting2 = vec![(0, vec![0, 1, 4]), (1, vec![5, 15566, 3423565]), (5, vec![0, 24, 56])];
+        let posting3 =
+            vec![(15, vec![24, 745, 6946]), (37, vec![234, 2356, 12345]), (98, vec![0, 1, 2, 3])];
+        assert!(create_dir_all(Path::new("/tmp/test_index2")).is_ok());
+        {
+            let mut prov1 = FsPostingStorage::new(Path::new("/tmp/test_index2"));
+            assert!(prov1.store(0, posting1.clone()).is_ok());
+            assert!(prov1.store(1, posting2.clone()).is_ok());
+        }
+
+        {
+            let mut prov2 = FsPostingStorage::from_folder(Path::new("/tmp/test_index2"));
+            assert_eq!(prov2.get(0).unwrap().as_ref(), &posting1);
+            assert_eq!(prov2.get(1).unwrap().as_ref(), &posting2);
+            assert!(prov2.store(2, posting3.clone()).is_ok());
+            assert_eq!(prov2.get(2).unwrap().as_ref(), &posting3);
+        }
+
+        {
+            let prov3 = FsPostingStorage::from_folder(Path::new("/tmp/test_index2"));
+            assert_eq!(prov3.get(0).unwrap().as_ref(), &posting1);
+            assert_eq!(prov3.get(1).unwrap().as_ref(), &posting2);
+            assert_eq!(prov3.get(2).unwrap().as_ref(), &posting3);
+        }
+    }
 }
