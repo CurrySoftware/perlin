b888299146102884947273480f6b217fbf533ab1
Jakob Demler <jdemler@curry-software.com>
2016-09-02 12:00:00 +0200

    Basic Analyzer

---
+++ src/utils/language.rs
@@ -0,0 +1,33 @@
+/// Analyzes a string and returns a vector of terms
+/// Tokenizes at non-alphanumerical characters and turns
+/// the tokens to lowercase.
+/// Example: "hans. .PETER!" will be transformed into ["hans", "peter"]
+pub fn basic_analyzer(input: &str) -> Vec<String> {
+    input.split(|c: char| !c.is_alphanumeric())
+        .filter(|token| !token.is_empty())
+        .map(|term| term.to_lowercase())
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::basic_analyzer;
+
+    #[test]
+    fn lowercase() {
+        assert_eq!(basic_analyzer("HANS"), vec!["hans"]);
+    }
+
+    #[test]
+    fn tokenization() {
+        assert_eq!(basic_analyzer("one small step for man"),
+                   vec!["one", "small", "step", "for", "man"]);
+    }
+
+    #[test]
+    fn non_alphanum() {
+        assert_eq!(basic_analyzer("!I!wouldn't: mind :SOme? ?BoiLed%Eggs<()>pLEAse"),
+                   vec!["i", "wouldn", "t", "mind", "some", "boiled", "eggs", "please"]);
+    }
+    
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,3 +4,4 @@ pub mod compression;
 pub mod owning_iterator;
 pub mod byte_code;
 pub mod persistence;
+pub mod language;
