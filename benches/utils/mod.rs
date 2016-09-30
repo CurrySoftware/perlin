9a4ac684b36f7648c978b2e3f824fee222c720fe
Jakob Demler <jdemler@curry-software.com>
2016-09-30 09:29:54 +0200

    merged indexing benchmarks from other fbs

--- benches/utils/mod.rs
+++ benches/utils/mod.rs
@@ -1,32 +1,41 @@
 use perlin::index::boolean_index::{IndexBuilder, BooleanIndex};
-use perlin::index::storage::RamStorage;
+use perlin::storage::RamStorage;
 use rand;
+use rand::{XorShiftRng, Rng};
 
 pub fn prepare_index(documents: usize, document_size: usize) -> BooleanIndex<usize> {
-    let rng = ZipfGenerator::new(voc_size(45, 0.5, documents*document_size));
+    let rng = ZipfGenerator::new(voc_size(45, 0.5, documents * document_size));
+    let collection: Vec<Vec<usize>> = (0..documents)
+        .map(|_| ZipfGenerator::new(voc_size(45, 0.5, documents * document_size)).take(document_size).collect())
+        .collect();
+    println!("Preparing index with {} documents each {} terms",
+             documents,
+             document_size);
     let index = IndexBuilder::<_, RamStorage<_>>::new()
-        .create((0..documents).map(|_| rng.take(document_size)));
+        .create(collection.iter().map(|i| i.iter().cloned()));
     index.unwrap()
 }
 
 // Implementation of Heaps' Law
-fn voc_size(k: usize, b: f64, tokens: usize) -> usize {
+pub fn voc_size(k: usize, b: f64, tokens: usize) -> usize {
     ((k as f64) * (tokens as f64).powf(b)) as usize
 }
 
 #[derive(Clone)]
-struct ZipfGenerator {
+pub struct ZipfGenerator {
     voc_size: usize,
     factor: f64,
-    acc_probs: Box<Vec<f64>>,
+    acc_probs: Vec<f64>,
+    rng: XorShiftRng,
 }
 
 impl ZipfGenerator {
-    fn new(voc_size: usize) -> Self {
+    pub fn new(voc_size: usize) -> Self {
         let mut res = ZipfGenerator {
             voc_size: voc_size,
             factor: (1.78 * voc_size as f64).ln(),
-            acc_probs: Box::new(Vec::with_capacity(voc_size)),
+            acc_probs: Vec::with_capacity(voc_size),
+            rng: rand::weak_rng(),
         };
         let mut acc = 0.0;
         for i in 1..voc_size {
@@ -37,11 +46,11 @@ impl ZipfGenerator {
     }
 }
 
-impl<'a> Iterator for &'a ZipfGenerator {
+impl Iterator for ZipfGenerator {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let dice = rand::random::<f64>();
+        let dice = self.rng.gen::<f64>();
         let mut c = 0;
         loop {
             if dice < self.acc_probs[c] {
