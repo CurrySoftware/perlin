af2aca43daecaea1dda0f80d01c473ec62962397
Jakob Demler <jdemler@curry-software.com>
2016-11-09 11:53:45 +0100

    Refactored ChunkRef and MutChunkRef

---
+++ src/chunked_storage/chunk_ref.rs
@@ -0,0 +1,105 @@
+use std::io;
+
+use storage::{Storage, StorageError};
+
+use chunked_storage::indexing_chunk::{IndexingChunk, HotIndexingChunk};
+use chunked_storage::SIZE;
+
+
+
+
+pub struct MutChunkRef<'a> {
+    chunk: &'a mut HotIndexingChunk,
+    archive: &'a mut Box<Storage<IndexingChunk>>,
+}
+
+pub struct ChunkRef<'a> {
+    read_ptr: usize,
+    chunk: &'a HotIndexingChunk,
+    archive: &'a Box<Storage<IndexingChunk>>,
+}
+
+
+
+// The idea here is the abstract the inner workings of
+// chunked storage and indexing chunk from the index
+// To do this, we implement Read and Write
+impl<'a> io::Write for MutChunkRef<'a> {
+    // Fill the HotIndexingChunk
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let bytes_written = try!((&mut self.chunk.data[SIZE - self.chunk.capacity as usize..]).write(buf));
+        self.chunk.capacity -= bytes_written as u16;
+        if self.chunk.capacity == 0 {
+            let id = self.archive.len();
+
+            match self.archive.store(id as u64, self.chunk.archive(id as u32)) {
+                Ok(_) => {}
+                Err(StorageError::IO(error)) |
+                Err(StorageError::ReadError(Some(error))) |
+                Err(StorageError::WriteError(Some(error))) => return Err(error),
+                _ => return Err(io::Error::last_os_error()),
+            }
+        }
+        Ok(bytes_written)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> io::Read for ChunkRef<'a> {
+    // BULLSHIT. RETHINK THIS!
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let mut bytes_read = 0;
+        loop {
+            let chunk_index = self.read_ptr / SIZE;
+            if chunk_index < self.chunk.archived_chunks().len() {
+                let chunk_id = self.chunk.archived_chunks()[chunk_index];
+                let read = (&self.archive.get(chunk_id as u64).unwrap().get_bytes()[self.read_ptr % SIZE..])
+                    .read(&mut buf[bytes_read..])
+                    .unwrap();
+                if read == 0 {
+                    return Ok(bytes_read);
+                }
+                bytes_read += read;
+                self.read_ptr += read;
+            } else {
+                break;
+            }
+        }
+        let read = (&self.chunk.get_bytes()[self.read_ptr % SIZE..]).read(&mut buf[bytes_read..]).unwrap();
+        bytes_read += read;
+        self.read_ptr += read;
+        Ok(bytes_read)
+    }
+}
+
+impl<'a> ChunkRef<'a> {
+
+    pub fn new(chunk: &'a HotIndexingChunk, archive: &'a Box<Storage<IndexingChunk>>) -> Self {
+        ChunkRef {
+            read_ptr: 0,
+            chunk: chunk,
+            archive: archive            
+        }
+    }
+    
+}
+
+impl<'a> MutChunkRef<'a> {
+    pub fn new(chunk: &'a mut HotIndexingChunk, archive: &'a mut Box<Storage<IndexingChunk>>) -> Self {
+        MutChunkRef {
+            chunk: chunk,
+            archive: archive,
+        }
+    }
+
+    pub fn get_last_doc_id(&self) -> u64 {
+        self.chunk.get_last_doc_id()
+    }
+
+    pub fn set_last_doc_id(&mut self, new_id: u64) {
+        self.chunk.set_last_doc_id(new_id);
+    }
+}
--- src/chunked_storage/mod.rs
+++ src/chunked_storage/mod.rs
@@ -1,94 +1,20 @@
-use std::io;
 use std::io::Write;
 use std::fs::OpenOptions;
 use std::path::Path;
 
-use storage::{Storage, StorageError, Result, ByteEncodable, ByteDecodable};
+use storage::{Storage, Result, ByteEncodable, ByteDecodable};
 use chunked_storage::indexing_chunk::HotIndexingChunk;
+use chunked_storage::chunk_ref::{ChunkRef, MutChunkRef};
 pub use chunked_storage::indexing_chunk::IndexingChunk;
 
 
 mod indexing_chunk;
+pub mod chunk_ref;
 
-pub const SIZE: usize = 104;
+pub const SIZE: usize = 222;
 const HOTCHUNKS_FILENAME: &'static str = "hot_chunks.bin";
 const ASSOCIATED_FILES: &'static [&'static str; 1] = &[HOTCHUNKS_FILENAME];
 
-
-pub struct MutChunkRef<'a> {
-    chunk: &'a mut HotIndexingChunk,
-    archive: &'a mut Box<Storage<IndexingChunk>>,
-}
-
-pub struct ChunkRef<'a> {
-    read_ptr: usize,
-    chunk: &'a HotIndexingChunk,
-    archive: &'a Box<Storage<IndexingChunk>>,
-}
-
-// The idea here is the abstract the inner workings of chunked storage and indexing chunk from the index
-// To do this, we implement Read and Write
-impl<'a> io::Write for MutChunkRef<'a> {
-    // Fill the HotIndexingChunk
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let bytes_written = try!((&mut self.chunk.data[SIZE - self.chunk.capacity as usize..]).write(buf));
-        self.chunk.capacity -= bytes_written as u16;
-        if self.chunk.capacity == 0 {
-            let id = self.archive.len();
-
-            match self.archive.store(id as u64, self.chunk.archive(id as u32)) {
-                Ok(_) => {}
-                Err(StorageError::IO(error)) |
-                Err(StorageError::ReadError(Some(error))) |
-                Err(StorageError::WriteError(Some(error))) => return Err(error),
-                _ => return Err(io::Error::last_os_error()),
-            }
-        }
-        Ok(bytes_written)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-impl<'a> io::Read for ChunkRef<'a> {
-    // BULLSHIT. RETHINK THIS!
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let mut bytes_read = 0;
-        loop {
-            let chunk_index = self.read_ptr / SIZE;            
-            if chunk_index < self.chunk.archived_chunks().len() {
-                let chunk_id = self.chunk.archived_chunks()[chunk_index];
-                let read = (&self.archive.get(chunk_id as u64).unwrap().get_bytes()[self.read_ptr % SIZE..])
-                    .read(&mut buf[bytes_read..])
-                    .unwrap();
-                if read == 0 {
-                    return Ok(bytes_read);
-                }
-                bytes_read += read;
-                self.read_ptr += read;
-            } else {
-                break;
-            }
-        }
-        let read = (&self.chunk.get_bytes()[self.read_ptr % SIZE..]).read(&mut buf[bytes_read..]).unwrap();
-        bytes_read += read;
-        self.read_ptr += read;
-        Ok(bytes_read)
-    }
-}
-
-impl<'a> MutChunkRef<'a> {
-    #[inline]
-    pub fn get_last_doc_id(&self) -> u64 {
-        self.chunk.get_last_doc_id()
-    }
-
-    pub fn set_last_doc_id(&mut self, new_id: u64) {
-        self.chunk.set_last_doc_id(new_id);
-    }
-}
 
 // TODO: Think about implementing `Persistent` and `Volatile`
 pub struct ChunkedStorage {
@@ -141,10 +67,7 @@ impl ChunkedStorage {
         }
 
         self.hot_chunks[term_id as usize] = HotIndexingChunk::new();
-        MutChunkRef {
-            chunk: &mut self.hot_chunks[term_id as usize],
-            archive: &mut self.archive,
-        }
+        MutChunkRef::new(&mut self.hot_chunks[term_id as usize], &mut self.archive)
     }
 
     #[inline]
@@ -154,19 +77,12 @@ impl ChunkedStorage {
 
     #[inline]
     pub fn get_current(&self, id: u64) -> ChunkRef {
-        ChunkRef {
-            read_ptr: 0,
-            chunk: &self.hot_chunks[id as usize],
-            archive: &self.archive,
-        }
+        ChunkRef::new(&self.hot_chunks[id as usize], &self.archive)        
     }
 
     #[inline]
     pub fn get_current_mut(&mut self, id: u64) -> MutChunkRef {
-        MutChunkRef {
-            chunk: &mut self.hot_chunks[id as usize],
-            archive: &mut self.archive,
-        }
+        MutChunkRef::new(&mut self.hot_chunks[id as usize], &mut self.archive)        
     }
 
 
@@ -291,7 +207,7 @@ mod tests {
         }
     }
 
-        #[test]
+    #[test]
     fn repeated_reads_overflowing() {
         let mut store = ChunkedStorage::new(10, Box::new(RamStorage::new()));
         store.new_chunk(0);
@@ -305,7 +221,8 @@ mod tests {
             let mut chunk_ref = store.get_current(0);
             for i in 0..100 {
                 chunk_ref.read(&mut read_data).unwrap();
-                assert_eq!(read_data.to_vec(), (i*10..i*10+10).map(|i| (i % 255) as u8).collect::<Vec<_>>());
+                assert_eq!(read_data.to_vec(),
+                           (i * 10..i * 10 + 10).map(|i| (i % 255) as u8).collect::<Vec<_>>());
             }
         }
     }
