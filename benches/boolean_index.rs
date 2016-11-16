d078825640ed23a2cb1fa10867684178ad6edad6
Jakob Demler <jdemler@curry-software.com>
2016-11-16 17:42:12 +0100

    expand benches for querying

--- benches/boolean_index.rs
+++ benches/boolean_index.rs
@@ -17,7 +17,7 @@ lazy_static!{
     pub static ref INDICES: [BooleanIndex<usize>; 3] = [
         prepare_index(1_000, 500), //1.000 documents with 500 terms a 8bytes = 4MB
         prepare_index(10_000, 500), //10.000 documents with 500 terms a 8bytes = 40MB
-        prepare_index(1_000, 500), //100.000 documents with 5000 terms a 8bytes = 400MB
+        prepare_index(1_000, 5000), //100.000 documents with 1000 terms a 8bytes = 800MB
     ];
 }
 
@@ -40,7 +40,7 @@ macro_rules! bench {
         }
 
         #[bench]
-        fn huge_collection_first_ten(b: &mut Bencher) {
+        fn large_documents_first_ten(b: &mut Bencher) {
             b.iter(||
                 black_box(INDICES[2].execute_query($query).take(10).count())
             );
@@ -61,7 +61,7 @@ macro_rules! bench {
         }
 
         #[bench]
-        fn huge_collection(b: &mut Bencher) {
+        fn large_documents_collection(b: &mut Bencher) {
             b.iter(||
                 black_box(INDICES[2].execute_query($query).count())
             );
@@ -84,5 +84,8 @@ macro_rules! build_bench {
 
 }
 
+build_bench!(and_seldom_seldom, &QueryBuilder::and(QueryBuilder::atoms(vec![4000, 4002])).build());
+build_bench!(and_frequent_seldom, &QueryBuilder::and(QueryBuilder::atoms(vec![4, 4002])).build());
+build_bench!(and_frequent_frequent, &QueryBuilder::and(QueryBuilder::atoms(vec![4, 5])).build());
 build_bench!(atom_frequent, &QueryBuilder::atom(4).build());
 build_bench!(atom_seldom, &QueryBuilder::atom(4000).build());
