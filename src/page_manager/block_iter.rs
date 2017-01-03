37e70808eb68d38f1c2a60b247ac9a63a9891f19
Jakob Demler <jdemler@curry-software.com>
2017-01-03 12:38:45 +0100

    testcase for bug in block_iter

--- src/page_manager/block_iter.rs
+++ src/page_manager/block_iter.rs
@@ -111,6 +111,58 @@ mod tests {
         assert_eq!(iter.next(), None);
     }
 
+
+    #[test]
+    fn full_unfull() {
+        let mut cache = new_cache("full_unfull");
+        // Fill with full pages
+        for i in 0..2048 {
+            assert_eq!(cache.store_block(Block([(i % 255) as u8; BLOCKSIZE])),
+                       PageId(i));
+            for j in 1..PAGESIZE {
+                cache.store_in_place(PageId(i),
+                                     BlockId(j as u16),
+                                     Block([(j % 255) as u8; BLOCKSIZE]));
+            }
+            cache.flush_page(PageId(i));
+        }
+        // Add Unfull page
+        assert_eq!(cache.store_block(Block([1; BLOCKSIZE])), PageId(2048));
+        assert_eq!(cache.flush_unfull(PageId(2048), BlockId(1)),
+                   UnfullPage::new(PageId(2048), BlockId(1), BlockId(2)));
+        let mut iter =
+            BlockIter::new(&cache,
+                           Pages((0..2048).map(|i| PageId(i)).collect::<Vec<_>>(),
+                                 Some(UnfullPage::new(PageId(2048), BlockId(1), BlockId(2)))));
+        // Iterate over full pages
+        for i in 0..2048 {
+            assert_eq!(iter.next(), Some(Block([(i % 255) as u8; BLOCKSIZE])));
+            for j in 1..PAGESIZE {
+                assert_eq!(iter.next(), Some(Block([(j % 255) as u8; BLOCKSIZE])));
+            }
+        }
+        // Unfull page
+        assert_eq!(iter.next(), Some(Block([1; BLOCKSIZE])));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn filled_unfull() {
+        let mut cache = new_cache("filled_unfull");
+        assert_eq!(cache.store_block(Block([0; BLOCKSIZE])), PageId(0));
+        for i in 1..PAGESIZE - 1 {
+            cache.store_in_place(PageId(0),
+                                 BlockId(i as u16),
+                                 Block([(i % 255) as u8; BLOCKSIZE]));            
+        }
+        let unfull_page = cache.flush_unfull(PageId(0), BlockId::last());
+        let mut iter = BlockIter::new(&cache, Pages(Vec::new(), Some(unfull_page)));
+        for i in 0..PAGESIZE -1 {
+            assert_eq!(iter.next(), Some(Block([(i % 255) as u8; BLOCKSIZE])));
+        }
+        assert_eq!(iter.next(), None);
+    }
+
     #[test]
     fn multiple_readers() {
         let mut cache = new_cache("basic");
