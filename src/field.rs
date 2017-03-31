11af69416bff36c236678cf1c220f313e7f1e814
Jakob Demler <jdemler@curry-software.com>
2017-03-31 12:41:09 +0200

    impl PipelineBucket for Field

--- src/field.rs
+++ src/field.rs
@@ -1,7 +1,20 @@
 use std::sync::Arc;
+use std::hash::Hash;
     
 use perlin_core::index::Index;
+use perlin_core::index::posting::DocId;
+
+use language::PipelineBucket;
 
 pub type Field<T> = Index<T>;
 
 pub type FacetField<T> = Index<Arc<T>>;
+
+
+impl<TTerm> PipelineBucket<TTerm> for Field<TTerm>
+    where TTerm: Hash + Eq + Ord
+{
+    fn put(&mut self, doc_id: DocId, term: TTerm) {
+        self.index_term(doc_id, term)
+    }
+}
